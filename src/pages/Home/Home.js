1c949eae21e13abf154b1c2612e50c250ee9ae6b
velo1203 <hokitoki1203@gmail.com>
2024-01-30 22:48:26 +0900

    changed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,24 +1,22 @@
-import {Button} from '../../components/Common/Button/Button';
-import { CheckBox } from '../../components/Common/CheckBox/CheckBox';
-import { Input } from '../../components/Common/Input/Input';
-import { Select } from '../../components/Common/Select/Select';
-import { TextArea } from '../../components/Common/TextArea/TextArea';
-import {StyledDefaultPage} from '../../style/layout/StyledDefaultPage';
+import { Button } from "../../components/Common/Button/Button";
+import { CheckBox } from "../../components/Common/CheckBox/CheckBox";
+import { Input } from "../../components/Common/Input/Input";
+import { Select } from "../../components/Common/Select/Select";
+import { TextArea } from "../../components/Common/TextArea/TextArea";
+import { StyledDefaultPage } from "../../style/layout/StyledDefaultPage";
 
 function Home() {
     return (
-    <StyledDefaultPage>
-
-        <Button>Button</Button>
-        <Input placeholder="Input" type="text"/>
-        <TextArea placeholder="TextArea" type="text" spellCheck={false}/>
-        <Select>
-            <option value="1">Select</option>
-            <option value="2">Select</option>
-            <option value="3">Select</option>
-        </Select>
-        <CheckBox type="checkbox" />
-    </StyledDefaultPage>
+        <StyledDefaultPage>
+            <Button>Button</Button>
+            <Input placeholder="Input" type="text" />
+            <TextArea placeholder="TextArea" type="text" spellCheck={false} />
+            <Select>
+                <option value="1">Select</option>
+                <option value="2">Select</option>
+            </Select>
+            <CheckBox type="checkbox" />
+        </StyledDefaultPage>
     );
 }
 
